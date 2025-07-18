bd2fee332b4b726605ed5328cafa3043c71af0c3
Rishab <f20211073@hyderabad.bits-pilani.ac.in>
2025-07-18 20:29:07 +0530

    Remove all files related to the Chrome extension to rewrite it using vite and typescript

--- index.js
+++
@@ -1,8 +0,0 @@
-async function sayHello() {
-    let [tab] = await chrome.tabs.query({ active: true});
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function: () => alert('Hello from the Chrome extension!')
-    });
-}
-document.getElementById('myButton').addEventListener('click', sayHello);
